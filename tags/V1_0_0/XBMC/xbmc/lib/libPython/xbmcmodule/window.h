21b47a0c26782d00db1ca29724e77eb58c7519c9
nobody <nobody@568bbfeb-2a22-0410-94d2-cc84cf5bfa90>
2004-06-29 09:25:31 +0000

    This commit was manufactured by cvs2svn to create tag 'V1_0_0'.

---
+++ tags/V1_0_0/XBMC/xbmc/lib/libPython/xbmcmodule/window.h
@@ -0,0 +1,31 @@
+#include "..\python.h"
+#include "GUIPythonWindow.h"
+
+#include "control.h"
+#include <vector>
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+namespace PYXBMC
+{
+	typedef struct {
+    PyObject_HEAD
+		int iWindowId;
+		int iOldWindowId;
+		int iCurrentControlId;
+		bool bIsPythonWindow;
+		bool bModal;
+		CGUIWindow* pWindow;
+		std::vector<Control*> vecControls;
+	} Window;
+
+	extern PyMethodDef Window_methods[];
+	extern PyTypeObject Window_Type;
+}
+
+#ifdef __cplusplus
+}
+#endif
