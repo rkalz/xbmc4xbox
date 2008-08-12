bb1d39d1f04d888daba2d1349e7882ed2c8a8f90
spiff_ <spiff_@568bbfeb-2a22-0410-94d2-cc84cf5bfa90>
2008-08-12 09:11:07 +0000

    remove old tag

--- tags/Atlantis_alpha3-linux-osx-win32/XBMC/tools/PackageMaker/post_install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Copy default sources if it doesn't exist
-mkdir -p "$HOME/Library/Application Support/XBMC/userdata"
-if [ ! -f  "$HOME/Library/Application Support/XBMC/userdata/sources.xml" ] ; then
-    cp "/var/tmp/sources.xml" "$HOME/Library/Application Support/XBMC/userdata"
-fi
