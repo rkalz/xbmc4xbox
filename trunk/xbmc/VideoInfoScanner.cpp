rkalz/xbmc4xbox

commit ee7a2c3b4713b463f578412389adfa64f016abea

changed: ticket #7541 - prioritize xbmc nfo's before mymovies.xml
git-svn-id: https://svn.exotica.org.uk:8443/xbmc4xbox@24287 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

trunk/xbmc/VideoInfoScanner.cpp
@@ -1200,13 +1200,8 @@ namespace VIDEO
         return GetnfoFile(&item2,bGrabAny);
       }
 
-      // mymovies.xml precedes any nfo file
       CStdString strPath;
       CUtil::GetDirectory(item->m_strPath,strPath);
-      nfoFile = CUtil::AddFileToFolder(strPath,"mymovies.xml");
-      if (CFile::Exists(nfoFile) && !item->m_bIsFolder) // "our" mymovies.xml only work for movies, not shows (mymovies.dk vs windows mce
-        return nfoFile;
-
       nfoFile = CUtil::AddFileToFolder(strPath,"movie.nfo");
       if (CFile::Exists(nfoFile))
         return nfoFile;
@@ -1253,6 +1248,11 @@ namespace VIDEO
           return GetnfoFile(&item2,bGrabAny);
         }
       }
+      
+      // finally try mymovies.xml
+      nfoFile = CUtil::AddFileToFolder(strPath,"mymovies.xml");
+      if (CFile::Exists(nfoFile))
+        return nfoFile;
     }
     if (item->m_bIsFolder || (bGrabAny && nfoFile.IsEmpty()))
     {

commit 7a54c5c485b82e31dcf4dd620b7542d36c4a02fa

fixed: regression after r24287

trunk/xbmc/VideoInfoScanner.cpp
@@ -1247,12 +1247,12 @@ namespace VIDEO
           CUtil::AddFileToFolder(strPath,CUtil::GetFileName(item->m_strPath),item2.m_strPath);
           return GetnfoFile(&item2,bGrabAny);
         }
+
+        // finally try mymovies.xml
+        nfoFile = CUtil::AddFileToFolder(strPath,"mymovies.xml");
+        if (CFile::Exists(nfoFile))
+          return nfoFile;
       }
-      
-      // finally try mymovies.xml
-      nfoFile = CUtil::AddFileToFolder(strPath,"mymovies.xml");
-      if (CFile::Exists(nfoFile))
-        return nfoFile;
     }
     if (item->m_bIsFolder || (bGrabAny && nfoFile.IsEmpty()))
     {