cc212c8aae95c342ce45ddb3aa30bf70213bd6ec
bobo1on1 <bobo1on1@568bbfeb-2a22-0410-94d2-cc84cf5bfa90>
2009-12-24 20:14:23 +0000

    removed stale branch

--- branches/prettylights/project/VS2008Express/update_svn_rev.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-rem subwcrev is included in the tortoise svn client: http://tortoisesvn.net/downloads
-SET REV_FILE=..\..\xbmc\win32\svn_rev.h
-IF EXIST %REV_FILE% (
-  del %REV_FILE%
-)
-subwcrev ../.. ../../xbmc/win32/svn_rev.tmpl %REV_FILE%
-SET REV_FILE=
