40d8826ba3b74c533013d74fa6de6af94692d633
alcoheca <alcoheca@568bbfeb-2a22-0410-94d2-cc84cf5bfa90>
2010-03-02 03:24:14 +0000

    Removing obsolete branch

--- branches/addons-fwork/project/Win32BuildSetup/dependencies/getdeps.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-rem copies additional dependencies from lib directories
-
-set CUR_PATH=%cd%
-cd "%~dp0"
-md ..\..\..\system\webserver > nul
-xcopy /Y ..\..\..\lib\libmicrohttpd_win32\bin\*.dll ..\..\..\system\webserver
-cd "%CUR_PATH%"
-set CUR_PATH=
--- branches/addons-fwork/xbmc/utils/FileUtils.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "FileItem.h"
-
-class CFileUtils
-{
-public:
-  static bool DeleteItem(const CFileItemPtr &item);
-  static bool RenameFile(const CStdString &strFile);
-};
