c79a31162e6834ccb51f6984cd2733b8a56fd5c1
althekiller <althekiller@568bbfeb-2a22-0410-94d2-cc84cf5bfa90>
2010-05-20 22:02:20 +0000

    Step 2 of 3, copy xbox branch to trunk

---
+++ trunk/guilib/update_svn_rev.bat
@@ -0,0 +1,19 @@
+@echo off
+rem subwcrev is included in the tortoise svn client: http://tortoisesvn.net/downloads
+SET REV_FILE=..\xbmc\xbox\svn_rev.h
+
+IF EXIST %REV_FILE% (
+  del %REV_FILE%
+)
+
+"%ProgramFiles%\TortoiseSVN\bin\subwcrev" .. ../xbmc/xbox/svn_rev.tmpl %REV_FILE% -f
+
+IF NOT EXIST %REV_FILE% (
+  subwcrev .. ../xbmc/xbox/svn_rev.tmpl %REV_FILE% -f
+)
+
+IF NOT EXIST %REV_FILE% (
+  copy ..\xbmc\xbox\svn_rev.unknown %REV_FILE%
+)
+
+SET REV_FILE=
