dfee423cb8651a906a3673ea0e29d0a25d16270e
exobuzz <exobuzz@568bbfeb-2a22-0410-94d2-cc84cf5bfa90>
2010-05-31 12:21:55 +0000

    changed: added codecs vp6a,vp6f to ffmpeg build (required by some streaming services)

--- branches/exobuzz/xbmc/cores/dvdplayer/Codecs/ffmpeg/build_xbmc_xbox.sh
+++ branches/exobuzz/xbmc/cores/dvdplayer/Codecs/ffmpeg/build_xbmc_xbox.sh
@@ -30,10 +30,10 @@ make distclean
 --enable-filter=buffer \
 \
 --disable-decoders \
---enable-decoder=aac,ac3,dca,flv,h263,h264,mp1,mp2,mp3,mpegvideo,mpeg1video,mpeg2video,mpeg4,svq1,svq3,vp5,vp6,vorbis,wmv1,wmv2,wmv3 \
+--enable-decoder=aac,ac3,dca,flv,h263,h264,mp1,mp2,mp3,mpegvideo,mpeg1video,mpeg2video,mpeg4,svq1,svq3,vp5,vp6,vp6a,vp6f,vorbis,wmv1,wmv2,wmv3 \
 \
 --disable-demuxers \
---enable-demuxer=aac,ac3,dts,asf,avi,flv,h263,h264,ogg,matroska,mp1,mp2,mp3,mpegps,mpegts,mpegvideo,mov \
+--enable-demuxer=aac,ac3,dts,asf,avi,flv,h263,h264,ogg,matroska,mp1,mp2,mp3,mpegps,mpegts,mpegtsraw,mpegvideo,mov \
 \
 --disable-protocols \
 --enable-protocol=file \
