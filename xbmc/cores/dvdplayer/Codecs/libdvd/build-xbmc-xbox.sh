b3d488be5be49e3509b9c07c78165f16b0201c1c
arnova <arnova@568bbfeb-2a22-0410-94d2-cc84cf5bfa90>
2010-07-07 13:35:34 +0000

    fixed: Typos in the libdvd build script

--- xbmc/cores/dvdplayer/Codecs/libdvd/build-xbmc-xbox.sh
+++ xbmc/cores/dvdplayer/Codecs/libdvd/build-xbmc-xbox.sh
@@ -28,7 +28,7 @@ echo "***** Building libdvdread *****"
 ./configure2 \
       --disable-shared \
       --enable-static \
-      --extra-cflags="-DHAVE_DVDCSS_DVDCSS_H -D_XBMC -DNDEBUG -D_MSC_VER -I`pwd`/../includes"
+      --extra-cflags="-DHAVE_DVDCSS_DVDCSS_H -D_XBMC -DNDEBUG -D_MSC_VER -I`pwd`/../includes" \
       --disable-debug
 mkdir -p ../includes/dvdread
 cp ../libdvdread/src/*.h ../includes/dvdread
@@ -44,7 +44,7 @@ echo "***** Building libdvdnav *****"
       --disable-shared \
       --enable-static \
       --extra-cflags="-D_XBMC -DNDEBUG -I`pwd`/../includes" \
-      --with-dvdread-config="`pwd`/../libdvdread/obj/dvdread-config"
+      --with-dvdread-config="`pwd`/../libdvdread/obj/dvdread-config" \
       --disable-debug
 make
 gcc \
