fb4a6d95c6b75cd69fffcbf992bc5f00904c40fb
arnova <arnova@568bbfeb-2a22-0410-94d2-cc84cf5bfa90>
2010-08-05 07:24:45 +0000

    added: Tickets #66,#67 and #68 (thanks xbs)

--- skin/Confluence/build.bat
+++ skin/Confluence/build.bat
@@ -12,7 +12,7 @@ Echo exclude.txt>>exclude.txt
 
 ECHO ----------------------------------------
 ECHO Creating XPR File...
-START /B /WAIT ..\..\Tools\XBMCTex\XBMCTex -input media -output media -noprotect
+START /B /WAIT XBMCTex -input media -output media -noprotect
 
 ECHO ----------------------------------------
 ECHO Copying XPR File...
@@ -26,6 +26,8 @@ ECHO ----------------------------------------
 ECHO XPR Texture Files Created...
 ECHO Building Skin Directory...
 xcopy "720p" "BUILD\Confluence\720p" /E /Q /I /Y /EXCLUDE:exclude.txt
+xcopy "NTSC16x9" "BUILD\Confluence\NTSC16x9" /E /Q /I /Y /EXCLUDE:exclude.txt
+xcopy "PAL16x9" "BUILD\Confluence\PAL16x9" /E /Q /I /Y /EXCLUDE:exclude.txt
 xcopy "fonts" "BUILD\Confluence\fonts" /E /Q /I /Y /EXCLUDE:exclude.txt
 xcopy "backgrounds" "BUILD\Confluence\backgrounds" /E /Q /I /Y /EXCLUDE:exclude.txt
 xcopy "sounds\*.*" "BUILD\Confluence\sounds\" /Q /I /Y /EXCLUDE:exclude.txt
@@ -37,4 +39,3 @@ del exclude.txt
 
 copy *.xml "BUILD\Confluence\"
 copy *.txt "BUILD\Confluence\"
- 	  	 
