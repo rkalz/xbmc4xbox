f39bd3c408ef783716067058b087b05b7b9583f5
arnova <arnova@568bbfeb-2a22-0410-94d2-cc84cf5bfa90>
2010-08-21 06:47:10 +0000

    reverted: reverted: merge of xbmc trunk/mainline r27170. Should fix ticket #59

--- xbmc/GUIWindowVideoBase.cpp
+++ xbmc/GUIWindowVideoBase.cpp
@@ -614,7 +614,7 @@ bool CGUIWindowVideoBase::ShowIMDB(CFileItem *item, const SScraperInfo& info2)
           }
         }
       }
-      if (returncode == -1)
+      else if (returncode == -1 || !CVideoInfoScanner::DownloadFailed(pDlgProgress))
       {
         pDlgProgress->Close();
         return false;
