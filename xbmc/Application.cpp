2200ecd29e8b31ae56a05e87d39d36bed4b0602e
arnova <arnova@568bbfeb-2a22-0410-94d2-cc84cf5bfa90>
2010-09-02 07:19:16 +0000

    fixed: That comment is no longer valid

--- xbmc/Application.cpp
+++ xbmc/Application.cpp
@@ -4245,7 +4245,6 @@ void CApplication::SaveFileState()
           CFileItemPtr msgItem(new CFileItem(*m_progressTrackingItem));
           if (m_progressTrackingItem->HasProperty("original_listitem_url"))
             msgItem->m_strPath = m_progressTrackingItem->GetProperty("original_listitem_url");
-          // Using this doesn't properly load fanart, so disable until we understand why
           CGUIMessage message(GUI_MSG_NOTIFY_ALL, g_windowManager.GetActiveWindow(), 0, GUI_MSG_UPDATE_ITEM, 1, msgItem); // 1 to update the listing as well 
           g_windowManager.SendThreadMessage(message);
         }
