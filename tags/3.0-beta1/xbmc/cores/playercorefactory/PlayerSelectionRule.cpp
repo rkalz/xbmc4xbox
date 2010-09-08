ebf50a0040d6ac18e7544bd278bc6fa8ab701baa
arnova <arnova@568bbfeb-2a22-0410-94d2-cc84cf5bfa90>
2010-09-08 16:00:09 +0000

    tagged: 3.0-beta1

---
+++ tags/3.0-beta1/xbmc/cores/playercorefactory/PlayerSelectionRule.cpp
@@ -0,0 +1,125 @@
+/*
+ *      Copyright (C) 2005-2010 Team XBMC
+ *      http://xbmc.org
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, write to
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  http://www.gnu.org/copyleft/gpl.html
+ *
+ */
+
+#include "stdafx.h"
+#include "URL.h"
+#include "PlayerSelectionRule.h"
+#include "utils/log.h"
+
+CPlayerSelectionRule::CPlayerSelectionRule(TiXmlElement* pRule)
+{
+  Initialize(pRule);
+}
+
+CPlayerSelectionRule::~CPlayerSelectionRule()
+{}
+
+void CPlayerSelectionRule::Initialize(TiXmlElement* pRule)
+{
+  m_name = pRule->Attribute("name");
+  if (!m_name || m_name.IsEmpty())
+    m_name = "un-named";
+
+  CLog::Log(LOGDEBUG, "CPlayerSelectionRule::Initialize: creating rule: %s", m_name.c_str());
+
+  m_tInternetStream = GetTristate(pRule->Attribute("internetstream"));
+  m_tAudio = GetTristate(pRule->Attribute("audio"));
+  m_tVideo = GetTristate(pRule->Attribute("video"));
+
+  m_tDVD = GetTristate(pRule->Attribute("dvd"));
+  m_tDVDFile = GetTristate(pRule->Attribute("dvdfile"));
+  m_tDVDImage = GetTristate(pRule->Attribute("dvdimage"));
+
+  m_protocols = pRule->Attribute("protocols");
+  m_fileTypes = pRule->Attribute("filetypes");
+  m_mimeTypes = pRule->Attribute("mimetypes");
+  m_fileName = pRule->Attribute("filename");
+
+  m_playerName = pRule->Attribute("player");
+  m_playerCoreId = 0;
+
+  TiXmlElement* pSubRule = pRule->FirstChildElement("rule");
+  while (pSubRule)
+  {
+    vecSubRules.push_back(new CPlayerSelectionRule(pSubRule));
+    pSubRule = pSubRule->NextSiblingElement("rule");
+  }
+}
+
+int CPlayerSelectionRule::GetTristate(const char* szValue) const
+{
+  if (szValue)
+  {
+    if (stricmp(szValue, "true") == 0) return 1;
+    if (stricmp(szValue, "false") == 0) return 0;
+  }
+  return -1;
+}
+
+void CPlayerSelectionRule::GetPlayers(const CFileItem& item, VECPLAYERCORES &vecCores)
+{
+  CLog::Log(LOGDEBUG, "CPlayerSelectionRule::GetPlayers: considering rule: %s", m_name.c_str());
+
+  if (m_tAudio >= 0 && (m_tAudio > 0) != item.IsAudio()) return;
+  if (m_tVideo >= 0 && (m_tVideo > 0) != item.IsVideo()) return;
+  if (m_tInternetStream >= 0 && (m_tInternetStream > 0) != item.IsInternetStream()) return;
+
+  if (m_tDVD >= 0 && (m_tDVD > 0) != item.IsDVD()) return;
+  if (m_tDVDFile >= 0 && (m_tDVDFile > 0) != item.IsDVDFile()) return;
+  if (m_tDVDImage >= 0 && (m_tDVDImage > 0) != item.IsDVDImage()) return;
+
+  CURL url(item.m_strPath);
+
+  CRegExp regExp;
+  if (m_fileTypes && m_fileTypes.length() > 0 && regExp.RegComp(m_fileTypes.c_str()))
+    if (regExp.RegFind(url.GetFileType(), 0) != 0) return;
+  
+  if (m_protocols && m_protocols.length() > 0 && regExp.RegComp(m_protocols.c_str()) &&
+      regExp.RegFind(url.GetProtocol(), 0) != 0) return;
+  
+  if (m_mimeTypes && m_mimeTypes.length() > 0 && regExp.RegComp(m_mimeTypes.c_str()) &&
+      regExp.RegFind(item.GetMimeType(), 0) != 0) return;
+
+  if (m_fileName && m_fileName.length() > 0 && regExp.RegComp(m_fileName.c_str()) &&
+      regExp.RegFind(item.m_strPath, 0) != 0) return;
+
+  CLog::Log(LOGDEBUG, "CPlayerSelectionRule::GetPlayers: matches rule: %s", m_name.c_str());
+
+  for (unsigned int i = 0; i < vecSubRules.size(); i++)
+    vecSubRules[i]->GetPlayers(item, vecCores);
+
+  PLAYERCOREID playerCoreId = GetPlayerCore();
+  if (playerCoreId != EPC_NONE)
+  {
+    CLog::Log(LOGDEBUG, "CPlayerSelectionRule::GetPlayers: adding player: %s (%d) for rule: %s", m_playerName.c_str(), playerCoreId, m_name.c_str());
+    vecCores.push_back(GetPlayerCore());
+  }
+}
+
+PLAYERCOREID CPlayerSelectionRule::GetPlayerCore()
+{
+  if (!m_playerCoreId)
+  {
+    m_playerCoreId = CPlayerCoreFactory::GetPlayerCore(m_playerName);
+  }
+  return m_playerCoreId;
+}
+
