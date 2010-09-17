058058ce630a53755e9334b74849a3aa925e5ec1
bombbloke <bombbloke@568bbfeb-2a22-0410-94d2-cc84cf5bfa90>
2010-09-17 09:41:14 +0000

    fixed: When compiling, SubWCRev & WinRaR paths were incorrectly detected in x64 environments.

--- trunk/guilib/update_svn_rev.bat
+++ trunk/guilib/update_svn_rev.bat
@@ -1,19 +1,32 @@
 @echo off
 rem subwcrev is included in the tortoise svn client: http://tortoisesvn.net/downloads
+
 SET REV_FILE=..\xbmc\xbox\svn_rev.h
 
 IF EXIST %REV_FILE% (
   del %REV_FILE%
 )
 
-"%ProgramFiles%\TortoiseSVN\bin\subwcrev" .. ../xbmc/xbox/svn_rev.tmpl %REV_FILE% -f
+IF EXIST "%ProgramFiles(x86)%\TortoiseSVN\bin\subwcrev.exe" SET SUBWCREV="%ProgramFiles(x86)%\TortoiseSVN\bin\subwcrev.exe"
+IF EXIST "%ProgramFiles%\TortoiseSVN\bin\subwcrev.exe"      SET SUBWCREV="%ProgramFiles%\TortoiseSVN\bin\subwcrev.exe"
+IF EXIST "%ProgramFilesW6432%\TortoiseSVN\bin\subwcrev.exe" SET SUBWCREV="%ProgramFilesW6432%\TortoiseSVN\bin\subwcrev.exe"
+
+IF NOT EXIST %SUBWCREV% (
+  ECHO SubWCRev not found, unable to embed correct revision number. Check TortoiseSVN install.
+  GOTO SKIPSUBWCREV
+)
+
+%SUBWCREV% .. ../xbmc/xbox/svn_rev.tmpl %REV_FILE% -f
 
 IF NOT EXIST %REV_FILE% (
-  subwcrev .. ../xbmc/xbox/svn_rev.tmpl %REV_FILE% -f
+  %SUBWCREV% .. ../xbmc/xbox/svn_rev.tmpl %REV_FILE% -f
 )
+
+:SKIPSUBWCREV
 
 IF NOT EXIST %REV_FILE% (
   copy ..\xbmc\xbox\svn_rev.unknown %REV_FILE%
 )
 
 SET REV_FILE=
+SET SUBWCREV=
