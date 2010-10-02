187124ca6f9f91d5c930924871a084f5f885c8b9
exobuzz <exobuzz@568bbfeb-2a22-0410-94d2-cc84cf5bfa90>
2010-10-02 05:11:13 +0000

    fixed: Seeking in files on cd broken on dvdplayer - the iso9660 filesystem did not handle the SEEK_POSSIBLE flag (returned 0), and so just returned the current file position (0) which dvdplayer took as seeking not supported. Fixed by returning 1 for SEEK_POSSIBLE queries for iso9660 filesystems.

--- trunk/xbmc/FileSystem/iso9660.h
+++ trunk/xbmc/FileSystem/iso9660.h
@@ -22,6 +22,7 @@
 #define ISO9660_H
 #pragma once
 #include "xbox/IoSupport.h"
+#include "IFile.h"
 #ifdef _WIN32
 // Ideally we should just be including iso9660.h, but it's not win32-ified at this point,
 // and these are all we need
