9c33e8cbb0e4ec3ec509bc0cf885592d1e115704
arnova <arnova@568bbfeb-2a22-0410-94d2-cc84cf5bfa90>
2010-10-20 14:39:49 +0000

    changed: Better handling in CNetwork::CheckNetwork() when link state changes

--- xbmc/xbox/Network.h
+++ xbmc/xbox/Network.h
@@ -56,7 +56,7 @@ public:
   // Return true if the magic packet was send
   bool WakeOnLan(char *mac); 
   
-  bool CheckNetwork(int count);
+  void CheckNetwork(int count);
   bool SetupNetwork();
   bool IsEthernetConnected();
   bool IsAvailable(bool wait = false);
