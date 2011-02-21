edad9e368b0f9509fa5375853f356424c9771ac7
arnova <arnova@568bbfeb-2a22-0410-94d2-cc84cf5bfa90>
2011-02-21 12:43:34 +0000

    merged: xbmc mainline commits (dvdplayer updates) 073d02d - fixed: account for DVDAudio internal buffer in GetCacheTime() f6ac5e2 - fixed: make sure CDVDAudio paused state is correctly set when creating the audio device e856308 - fixed: Ticket #11147 - can't parser srt subtitles with CRCRLF at line end 03e95c9 - added: Ticket #10696 - support ass tag Italics, Bold and Color in srt subtitles 9a2c049 - Allow better use of dvdnavigator's Prev+Next -- Ticket #9674 - update

--- xbmc/cores/dvdplayer/DVDAudio.cpp
+++ xbmc/cores/dvdplayer/DVDAudio.cpp
@@ -45,6 +45,8 @@ CDVDAudio::CDVDAudio(volatile bool &bStop)
   m_iBitsPerSample = 0;
   m_iBitrate = 0;
   m_iChannels = 0;
+  m_SecondsPerByte = 0.0;
+  m_bPaused = true;
 }
 
 CDVDAudio::~CDVDAudio()
@@ -114,6 +116,10 @@ bool CDVDAudio::Create(const DVDAudioFrame &audioframe, CodecID codec)
   m_iBitrate = audioframe.sample_rate;
   m_iBitsPerSample = audioframe.bits_per_sample;
   m_bPassthrough = audioframe.passthrough;
+  if(m_iChannels && m_iBitrate && m_iBitsPerSample)
+    m_SecondsPerByte = 1.0 / (m_iChannels * m_iBitrate * (m_iBitsPerSample>>3));
+  else
+    m_SecondsPerByte = 0.0;
 
   m_dwPacketSize = m_pAudioDecoder->GetChunkLen();
   if(m_bPaused)
@@ -143,6 +149,7 @@ void CDVDAudio::Destroy()
   m_iBitrate = 0;
   m_iBitsPerSample = 0;
   m_bPassthrough = false;
+  m_bPaused = true;
 }
 
 DWORD CDVDAudio::AddPacketsRenderer(unsigned char* data, DWORD len, CSingleLock &lock)
@@ -156,7 +163,7 @@ DWORD CDVDAudio::AddPacketsRenderer(unsigned char* data, DWORD len, CSingleLock 
 
   //Calculate a timeout when this definitely should be done
   double timeout;
-  timeout  = DVD_SEC_TO_TIME(m_pAudioDecoder->GetDelay() + (double)len / bps);
+  timeout  = DVD_SEC_TO_TIME(m_pAudioDecoder->GetDelay() + len * m_SecondsPerByte);
   timeout += DVD_SEC_TO_TIME(1.0);
   timeout += CDVDClock::GetAbsoluteClock();
 
@@ -308,9 +315,7 @@ double CDVDAudio::GetDelay()
   if(m_pAudioDecoder)
     delay = m_pAudioDecoder->GetDelay();
 
-  DWORD bps = m_iChannels * m_iBitrate * (m_iBitsPerSample>>3);
-  if(m_iBufferSize && bps)
-    delay += (double)m_iBufferSize / bps;
+  delay += m_SecondsPerByte * m_iBufferSize;
 
   return delay * DVD_TIME_BASE;
 }
@@ -348,7 +353,14 @@ double CDVDAudio::GetCacheTime()
   CSingleLock lock (m_critSection);
   if(!m_pAudioDecoder)
     return 0.0;
-  return m_pAudioDecoder->GetCacheTime();
+
+  double delay = 0.0;
+  if(m_pAudioDecoder)
+    delay = m_pAudioDecoder->GetCacheTime();
+
+  delay += m_SecondsPerByte * m_iBufferSize;
+
+  return delay;
 }
 
 double CDVDAudio::GetCacheTotal()
