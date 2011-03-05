ae3e7d3dd493dd40a99d9e1ac75e21246416fa46
arnova <arnova@568bbfeb-2a22-0410-94d2-cc84cf5bfa90>
2011-03-05 07:23:37 +0000

    fixed: Win32 build with patch from ticket #157 (thanks to wannabegt4)

--- trunk/xbmc/Application.cpp
+++ trunk/xbmc/Application.cpp
@@ -231,7 +231,6 @@ using namespace EVENTSERVER;
  #else
   #pragma comment (lib,"../../xbmc/lib/libGoAhead/goahead_win32d.lib") // SECTIONNAME=LIBHTTP
   #pragma comment (lib,"../../xbmc/lib/sqLite/libSQLite3_win32d.lib")
-  #pragma comment (lib,"../../xbmc/lib/libshout/libshout_win32d.lib" )
   #pragma comment (lib,"../../xbmc/lib/libcdio/libcdio_win32d.lib" )
   #pragma comment (lib,"../../xbmc/lib/libiconv/libiconvd.lib")
   #pragma comment (lib,"../../xbmc/lib/libfribidi/libfribidid.lib")
@@ -257,7 +256,6 @@ using namespace EVENTSERVER;
  #else
   #pragma comment (lib,"../../xbmc/lib/libGoAhead/goahead_win32.lib")
   #pragma comment (lib,"../../xbmc/lib/sqLite/libSQLite3_win32.lib")
-  #pragma comment (lib,"../../xbmc/lib/libshout/libshout_win32.lib" )
   #pragma comment (lib,"../../xbmc/lib/libcdio/libcdio_win32.lib" )
   #pragma comment (lib,"../../xbmc/lib/libiconv/libiconv.lib")
   #pragma comment (lib,"../../xbmc/lib/libfribidi/libfribidi.lib")
