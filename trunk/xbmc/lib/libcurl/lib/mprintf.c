9ca3b4c207f3668e6e2344e5bff248ddca01ff76
exobuzz <exobuzz@568bbfeb-2a22-0410-94d2-cc84cf5bfa90>
2011-05-16 00:50:33 +0000

    changed: dont use fputs/fputc calls that cause "dll_fputx emulated function failed" errors. Not 100% sure about the mprintf changes, but it seems to work fine (and the calls were failing before anyway).

--- trunk/xbmc/lib/libcurl/lib/mprintf.c
+++ trunk/xbmc/lib/libcurl/lib/mprintf.c
@@ -1170,7 +1170,7 @@ int curl_mprintf(const char *format, ...)
   va_list ap_save; /* argument pointer */
   va_start(ap_save, format);
 
-  retcode = dprintf_formatf(stdout, fputc, format, ap_save);
+  retcode = dprintf_formatf(stdout, addbyter, format, ap_save);
   va_end(ap_save);
   return retcode;
 }
@@ -1180,7 +1180,7 @@ int curl_mfprintf(FILE *whereto, const char *format, ...)
   int retcode;
   va_list ap_save; /* argument pointer */
   va_start(ap_save, format);
-  retcode = dprintf_formatf(whereto, fputc, format, ap_save);
+  retcode = dprintf_formatf(whereto, addbyter, format, ap_save);
   va_end(ap_save);
   return retcode;
 }
@@ -1195,12 +1195,12 @@ int curl_mvsprintf(char *buffer, const char *format, va_list ap_save)
 
 int curl_mvprintf(const char *format, va_list ap_save)
 {
-  return dprintf_formatf(stdout, fputc, format, ap_save);
+  return dprintf_formatf(stdout, addbyter, format, ap_save);
 }
 
 int curl_mvfprintf(FILE *whereto, const char *format, va_list ap_save)
 {
-  return dprintf_formatf(whereto, fputc, format, ap_save);
+  return dprintf_formatf(whereto, addbyter, format, ap_save);
 }
 
 #ifdef DPRINTF_DEBUG
