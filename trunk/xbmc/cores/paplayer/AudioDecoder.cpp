49f1a3110bc4f5a04cf20e96f6ee46619b33d00e
exobuzz <exobuzz@568bbfeb-2a22-0410-94d2-cc84cf5bfa90>
2011-11-29 04:13:43 +0000

    changed: resync AudioDecoder with xbmc mainline. cosmetic plus create buffer size change

--- trunk/xbmc/cores/paplayer/AudioDecoder.cpp
+++ trunk/xbmc/cores/paplayer/AudioDecoder.cpp
@@ -67,7 +67,8 @@ bool CAudioDecoder::Create(const CFileItem &file, __int64 seekOffset, unsigned i
 
   CSingleLock lock(m_critSection);
   // create our pcm buffer
-  m_pcmBuffer.Create( std::max<unsigned int>(1, nBufferSize) * INTERNAL_BUFFER_LENGTH );
+  m_pcmBuffer.Create((int)std::max<unsigned int>(2, nBufferSize) *
+                     INTERNAL_BUFFER_LENGTH);
 
   // reset our playback timing variables
   m_eof = false;
@@ -177,7 +178,7 @@ void CAudioDecoder::PrefixData(void *data, unsigned int size)
     CLog::Log(LOGERROR, "CAudioDecoder::PrefixData() failed - null data pointer");
     return;
   }
-  m_gaplessBufferSize = std::min((unsigned int) PACKET_SIZE, size);
+  m_gaplessBufferSize = std::min<unsigned int>(PACKET_SIZE, size);
   memcpy(m_gaplessBuffer, data, m_gaplessBufferSize*sizeof(float));
   if (m_gaplessBufferSize != size)
     CLog::Log(LOGWARNING, "CAudioDecoder::PrefixData - losing %i bytes of audio data in track transistion", size - m_gaplessBufferSize);
@@ -196,8 +197,9 @@ int CAudioDecoder::ReadSamples(int numsamples)
   CSingleLock lock(m_critSection);
 
   // Read in more data
-  int maxsize = std::min<unsigned int>(INPUT_SAMPLES, m_pcmBuffer.getMaxWriteSize() / sizeof(float));
-  numsamples = std::min(numsamples, maxsize);
+  int maxsize = std::min<int>(INPUT_SAMPLES,
+                  (m_pcmBuffer.getMaxWriteSize() / (int)(sizeof (float))));
+  numsamples = std::min<int>(numsamples, maxsize);
   numsamples -= (numsamples % m_codec->m_Channels);  // make sure it's divisible by our number of channels
   if ( numsamples )
   {
