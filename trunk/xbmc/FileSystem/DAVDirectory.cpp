f4d719dae69548d85cdc3d571bcde11af57e37de
exobuzz <exobuzz@568bbfeb-2a22-0410-94d2-cc84cf5bfa90>
2012-05-21 23:58:30 +0000

    fixed: webdav - less error prone response handling based on xbmc mainline ced7f32ea

--- trunk/xbmc/FileSystem/DAVDirectory.cpp
+++ trunk/xbmc/FileSystem/DAVDirectory.cpp
@@ -197,76 +197,56 @@ bool CDAVDirectory::GetDirectory(const CStdString& strPath, CFileItemList &items
     return false;
   }
 
-  char buffer[MAX_PATH + 1024];
   CStdString strResponse;
-  CStdString strHeader;
-  while (dav.ReadString(buffer, sizeof(buffer)))
+  dav.ReadData(strResponse);
+
+  TiXmlDocument davResponse;
+  davResponse.Parse(strResponse.c_str());
+
+  if (!davResponse.Parse(strResponse))
   {
-    if (strstr(buffer, "<D:response") != NULL)
-    {
-      // The header should contain the xml version/utf encoding line
-      // followed by the <multistatus> tag
-      if (strHeader.IsEmpty())
-        strHeader = strResponse;
-      
-      strResponse = strHeader;
-    }
-    strResponse.append(buffer, strlen(buffer));
+    CLog::Log(LOGERROR, "%s - Unable to process dav directory (%s)", __FUNCTION__, strPath.c_str());
+    dav.Close();
+    return false;
+  }
 
-    if (strstr(buffer, "</D:response") != NULL)
+  TiXmlNode *pChild;
+  // Iterate over all responses
+  for (pChild = davResponse.RootElement()->FirstChild(); pChild != 0; pChild = pChild->NextSibling())
+  {
+    if (ValueWithoutNamespace(pChild, "response"))
     {
-      // Close the multistatus tag from the header
-      if (strHeader.Find("<D:multistatus"))
-        strResponse+="</D:multistatus>\n";
-      
-      TiXmlDocument davResponse;
+      CFileItem item;
+      ParseResponse(pChild->ToElement(), item);
+      CURL url2(strPath);
+      CURL url3(item.GetPath());
 
-      if (!davResponse.Parse(strResponse))
+      CStdString itemPath(URIUtils::AddFileToFolder(url2.GetWithoutFilename(), url3.GetFileName()));
+
+      if (item.GetLabel().IsEmpty())
       {
-        CLog::Log(LOGERROR, "%s - Unable to process dav directory (%s)", __FUNCTION__, strPath.c_str());
-        dav.Close();
-        return false;
+        CStdString name(itemPath);
+        URIUtils::RemoveSlashAtEnd(name);
+        CURL::Decode(name);
+        item.SetLabel(URIUtils::GetFileName(name));
       }
 
-      TiXmlNode *pChild;
-      // Iterate over all responses
-      for (pChild = davResponse.RootElement()->FirstChild(); pChild != 0; pChild = pChild->NextSibling())
+      if (item.m_bIsFolder)
+        URIUtils::AddSlashAtEnd(itemPath);
+
+      // Add back protocol options
+      if (!url2.GetProtocolOptions().IsEmpty())
+        itemPath += "|" + url2.GetProtocolOptions();
+      item.SetPath(itemPath);
+
+      if (!item.GetPath().Equals(strPath))
       {
-        if (ValueWithoutNamespace(pChild, "response"))
-        {
-          CFileItem item;
-          ParseResponse(pChild->ToElement(), item);
-          CURL url2(strPath);
-          CURL url3(item.GetPath());
-
-          CStdString itemPath(URIUtils::AddFileToFolder(url2.GetWithoutFilename(), url3.GetFileName()));
-
-          if (item.GetLabel().IsEmpty())
-          {
-            CStdString name(itemPath);
-            URIUtils::RemoveSlashAtEnd(name);
-            CURL::Decode(name);
-            item.SetLabel(URIUtils::GetFileName(name));
-          }
-
-          if (item.m_bIsFolder)
-            URIUtils::AddSlashAtEnd(itemPath);
-
-          // Add back protocol options
-          if (!url2.GetProtocolOptions().IsEmpty())
-            itemPath += "|" + url2.GetProtocolOptions();
-          item.SetPath(itemPath);
-
-          if (!item.GetPath().Equals(strPath))
-          {
-            CFileItemPtr pItem(new CFileItem(item));
-            items.Add(pItem);
-          }
-        }
+        CFileItemPtr pItem(new CFileItem(item));
+        items.Add(pItem);
       }
-      strResponse.clear();
     }
   }
+
   dav.Close();
 
   return true;
