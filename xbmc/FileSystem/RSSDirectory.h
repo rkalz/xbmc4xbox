a49badfee01f87bf0ebb8be20220ed7768a81f3f
buzz <buzz@568bbfeb-2a22-0410-94d2-cc84cf5bfa90>
2012-08-01 01:26:35 +0000

    updated: resync RSSDirectory with xbmc mainline code - fixes issues such as wrong date display as well as .rss file support.

--- xbmc/FileSystem/RSSDirectory.h
+++ xbmc/FileSystem/RSSDirectory.h
@@ -22,11 +22,12 @@
 #ifndef CRSSDIRECTORY_H_
 #define CRSSDIRECTORY_H_
 
-#include "IDirectory.h"
+#include "IFileDirectory.h"
+#include "FileItem.h"
 
 namespace XFILE
 {
-  class CRSSDirectory : public IDirectory
+  class CRSSDirectory : public IFileDirectory
   {
   public:
     CRSSDirectory();
@@ -34,7 +35,12 @@ namespace XFILE
     virtual bool GetDirectory(const CStdString& strPath, CFileItemList &items);
     virtual bool Exists(const char* strPath);
     virtual bool IsAllowed(const CStdString &strFile) const { return true; };
-    virtual DIR_CACHE_TYPE GetCacheType(const CStdString& strPath) const { return DIR_CACHE_ALWAYS; };
+    virtual bool ContainsFiles(const CStdString& strPath);
+    virtual DIR_CACHE_TYPE GetCacheType(const CStdString& strPath) const { return DIR_CACHE_ONCE; };
+  protected:
+    // key is path, value is cache invalidation date
+    static std::map<CStdString,CDateTime> m_cache;
+    static CCriticalSection m_section;
   };
 }
 
