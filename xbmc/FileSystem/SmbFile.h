63c773aa3af0baceff8a9164edb593199586c5d7
buzz <buzz@568bbfeb-2a22-0410-94d2-cc84cf5bfa90>
2012-08-03 15:28:32 +0000

    changed: disable chunked reading for smb:// again due to problems with some iso's over smb:// - more debugging needed.

--- xbmc/FileSystem/SmbFile.h
+++ xbmc/FileSystem/SmbFile.h
@@ -94,7 +94,8 @@ public:
   virtual bool OpenForWrite(const CURL& url, bool bOverWrite = false);
   virtual bool Delete(const CURL& url);
   virtual bool Rename(const CURL& url, const CURL& urlnew);
-  virtual int  GetChunkSize() {return 1;}
+  //still has some problems with some iso's over smb://
+  //virtual int  GetChunkSize() {return 1;}
 
 protected:
   CURL m_url;
