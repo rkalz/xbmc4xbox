217ef21a9807d832bb0f52764cf179b319d1a961
buzz <buzz@568bbfeb-2a22-0410-94d2-cc84cf5bfa90>
2012-08-27 22:00:43 +0000

    merged: xbmc mainline a1421abb - fixed: Make sure source window ID is always initialized.

--- trunk/xbmc/guilib/GUIActionDescriptor.h
+++ trunk/xbmc/guilib/GUIActionDescriptor.h
@@ -19,12 +19,14 @@ public:
   {
     m_lang = LANG_XBMC;
     m_action = action;    
+    m_sourceWindowId = -1;
   }
   
   CGUIActionDescriptor(ActionLang lang, CStdString& action)
   {
     m_lang = lang;
     m_action = action;
+    m_sourceWindowId = -1;
   }
   
   CStdString m_action;
