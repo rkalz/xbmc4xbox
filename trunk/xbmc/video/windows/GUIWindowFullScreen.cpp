87491f748879c3b85b46cd31bb353acfdd6e1e18
buzz <buzz@568bbfeb-2a22-0410-94d2-cc84cf5bfa90>
2012-08-29 15:50:50 +0000

    merged: xbmc mainline 8d7151e5 - changed: Switch RenderOutline to use a separate border font, inflated by freetype rather than re-rendering the same text over and over.  Fixes #9713.

--- trunk/xbmc/video/windows/GUIWindowFullScreen.cpp
+++ trunk/xbmc/video/windows/GUIWindowFullScreen.cpp
@@ -835,7 +835,7 @@ void CGUIWindowFullScreen::RenderTTFSubtitles()
       float x = maxWidth * 0.5f + g_settings.m_ResInfo[res].Overscan.left;
       float y = g_settings.m_ResInfo[res].iSubtitles - textHeight;
 
-      m_subsLayout->RenderOutline(x, y, 0, 0xFF000000, 3, XBFONT_CENTER_X, maxWidth);
+      m_subsLayout->RenderOutline(x, y, 0, 0xFF000000, XBFONT_CENTER_X, maxWidth);
     }
   }
 }
