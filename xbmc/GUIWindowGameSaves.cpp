6befb35d28ba08e8f24b67537c088dd656c7b0ae
buzz <buzz@568bbfeb-2a22-0410-94d2-cc84cf5bfa90>
2012-09-12 17:23:38 +0000

    updated: copyright dates

--- xbmc/GUIWindowGameSaves.cpp
+++ xbmc/GUIWindowGameSaves.cpp
@@ -1,5 +1,5 @@
 /*
- *      Copyright (C) 2005-2008 Team XBMC
+ *      Copyright (C) 2005-2012 Team XBMC
  *
  *  This Program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
