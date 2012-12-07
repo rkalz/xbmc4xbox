74168a362da365003945ddeb9ae3f8038227726b
buzz <buzz@568bbfeb-2a22-0410-94d2-cc84cf5bfa90>
2012-12-07 21:02:18 +0000

    merged: xbmc mainline abf376f4 - refactor: Use CGUIDialogContextMenu::ShowAndGetChoice() rather than adding buttons one by one all over the show

--- xbmc/video/dialogs/GUIDialogVideoBookmarks.cpp
+++ xbmc/video/dialogs/GUIDialogVideoBookmarks.cpp
@@ -305,34 +305,22 @@ void CGUIDialogVideoBookmarks::AddEpisodeBookmark()
 {
   vector<CVideoInfoTag> episodes;
   CVideoDatabase videoDatabase;
-  CPoint pos;
   videoDatabase.Open();
   videoDatabase.GetEpisodesByFile(g_application.CurrentFile(), episodes);
   videoDatabase.Close();
   if(episodes.size() > 0)
   {
-    CGUIDialogContextMenu *pMenu = (CGUIDialogContextMenu *)g_windowManager.GetWindow(WINDOW_DIALOG_CONTEXT_MENU);
-    map<int, CVideoInfoTag*> buttons;
-    const CGUIControl *pList = GetControl(CONTROL_ADD_EPISODE_BOOKMARK);
-    if (pList)
-      pos = pList->GetRenderPosition() + CPoint(pList->GetWidth() * 0.5f, pList->GetHeight() * 0.5f);
-    if (pMenu)
+    CContextButtons choices;
+    for (unsigned int i=0; i < episodes.size(); ++i)
     {
-      pMenu->Initialize();
-      for (unsigned int i=0; i < episodes.size(); ++i)
-      {
-        CStdString strButton;
-        strButton.Format("%s %i, %s %i", g_localizeStrings.Get(20373), episodes[i].m_iSeason, g_localizeStrings.Get(20359).c_str(), episodes[i].m_iEpisode);
-        buttons[pMenu->AddButton(strButton)] = &episodes[i];
-      }
-
-      pMenu->OffsetPosition(pos.x, pos.y);
-      pMenu->DoModal(GetID());
-
-      int pressed = pMenu->GetButton();
-      if (buttons.find(pressed) != buttons.end())
-        AddBookmark(buttons[pressed]);
+      CStdString strButton;
+      strButton.Format("%s %i, %s %i", g_localizeStrings.Get(20373), episodes[i].m_iSeason, g_localizeStrings.Get(20359).c_str(), episodes[i].m_iEpisode);
+      choices.Add(i, strButton);
     }
+
+    int pressed = CGUIDialogContextMenu::ShowAndGetChoice(choices);
+    if (pressed >= 0)
+      AddBookmark(&episodes[pressed]);
   }
 }
 
