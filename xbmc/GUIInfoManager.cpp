d0c5a566c47a70dbf594fa5ba02083677d94a589
buzz <buzz@568bbfeb-2a22-0410-94d2-cc84cf5bfa90>
2013-03-03 12:20:20 +0000

    merged: xbmc mainline abf53090 -  gui: always hide seek dialog when player signals display after seek. This fixes stuck seek bar with paplayer

--- xbmc/GUIInfoManager.cpp
+++ xbmc/GUIInfoManager.cpp
@@ -3564,6 +3564,7 @@ CStdString CGUIInfoManager::GetBuild()
 
 void CGUIInfoManager::SetDisplayAfterSeek(unsigned int timeOut, int seekOffset)
 {
+  g_infoManager.m_performingSeek = false;
   if (timeOut>0)
   {
     m_AfterSeekTimeout = CTimeUtils::GetFrameTime() +  timeOut;
