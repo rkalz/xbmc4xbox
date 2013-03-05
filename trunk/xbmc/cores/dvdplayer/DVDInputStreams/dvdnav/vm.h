65009ac3045b932cb4dfe8950f1f8e0f5b8bd9c6
buzz <buzz@568bbfeb-2a22-0410-94d2-cc84cf5bfa90>
2013-03-05 19:58:00 +0000

    updated: xbmc copy of libdvd headers

--- trunk/xbmc/cores/dvdplayer/DVDInputStreams/dvdnav/vm.h
+++ trunk/xbmc/cores/dvdplayer/DVDInputStreams/dvdnav/vm.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2000, 2001 Håkan Hjort
+ * Copyright (C) 2000, 2001 HÃ¥kan Hjort
  * Copyright (C) 2001 Rich Wareham <richwareham@users.sourceforge.net>
  * 
  * This file is part of libdvdnav, a DVD navigation library. It is modified
@@ -15,16 +15,13 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
- *
- * $Id$
- *
+ * You should have received a copy of the GNU General Public License along
+ * with libdvdnav; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-#ifndef VM_H_INCLUDED
-#define VM_H_INCLUDED
+#ifndef LIBDVDNAV_VM_H
+#define LIBDVDNAV_VM_H
 
 #include "remap.h"
 #include "dvdnav_internal.h"
@@ -88,6 +85,7 @@ typedef struct {
   dvd_state_t   state;
   int32_t       hop_channel;
   char          dvd_name[50];
+  char          dvd_serial[15];
   remap_t      *map;
   int           stopped;
 } vm_t;
@@ -144,6 +142,7 @@ void vm_get_next_cell(vm_t *vm);
 int vm_jump_pg(vm_t *vm, int pg);
 int vm_jump_cell_block(vm_t *vm, int cell, int block);
 int vm_jump_title_part(vm_t *vm, int title, int part);
+int vm_jump_title_program(vm_t *vm, int title, int pgcn, int pgn);
 int vm_jump_top_pg(vm_t *vm);
 int vm_jump_next_pg(vm_t *vm);
 int vm_jump_prev_pg(vm_t *vm);
@@ -160,22 +159,28 @@ int vm_get_subp_stream(vm_t *vm, int subpN, int mode);
 int vm_get_audio_active_stream(vm_t *vm);
 int vm_get_subp_active_stream(vm_t *vm, int mode);
 void vm_get_angle_info(vm_t *vm, int *current, int *num_avail);
-// XBOX #if 0
-// XBOX /* currently unused */
 void vm_get_audio_info(vm_t *vm, int *current, int *num_avail);
 void vm_get_subp_info(vm_t *vm, int *current, int *num_avail);
 void vm_get_video_res(vm_t *vm, int *width, int *height);
-// XBOX #endif
 int  vm_get_video_aspect(vm_t *vm);
 int  vm_get_video_scale_permission(vm_t *vm);
 video_attr_t vm_get_video_attr(vm_t *vm);
 audio_attr_t vm_get_audio_attr(vm_t *vm, int streamN);
 subp_attr_t  vm_get_subp_attr(vm_t *vm, int streamN);
+ifo_handle_t *vm_get_title_ifo(vm_t *vm, uint32_t title);
+void vm_ifo_close(ifo_handle_t *ifo);
 
+int vm_get_state(vm_t *vm, dvd_state_t *save_state);
+int vm_set_state(vm_t *vm, dvd_state_t *save_state);
+
+/* Uncomment for VM command tracing */
+/* #define TRACE */
 #ifdef TRACE
 /* Debug */
 void vm_position_print(vm_t *vm, vm_position_t *position);
 #endif
 
+/* XBMC added functions */
+vm_t* dvdnav_get_vm(dvdnav_t *self);
 
-#endif /* VM_HV_INCLUDED */
+#endif /* LIBDVDNAV_VM_H */
