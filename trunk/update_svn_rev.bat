106175980bc9b006da27df02b78cfaaa42b9d0f7
buzz <buzz@568bbfeb-2a22-0410-94d2-cc84cf5bfa90>
2013-06-19 19:38:16 +0000

    fixed: update_svn_rev.bat didn't properly quote paths which caused breakage building with spaces in the build path. changed: clean up and comments for update_svn_rev.bat - thanks to nrjohnstone for the changes. redmine ticket #238

--- trunk/update_svn_rev.bat
+++ trunk/update_svn_rev.bat
@@ -1,26 +1,40 @@
 @echo off
-rem subwcrev is included in the tortoise svn client: http://tortoisesvn.net/downloads
+REM subwcrev is included in the tortoise svn client: http://tortoisesvn.net/downloads
 
+REM Current directory including drive
 SET CWD=%~dp0
 
-SET REV_FILE=%CWD%xbmc\xbox\svn_rev.h
+SET REV_FILE="%CWD%xbmc\xbox\svn_rev.h"
+SET SVN_TEMPLATE="%CWD%xbmc\xbox\svn_rev.tmpl"
 
 IF EXIST %REV_FILE% del %REV_FILE%
 
-SET SUBWCREV="?"
+SET SUBWCREV=""
+
 IF EXIST "%ProgramFiles(x86)%\TortoiseSVN\bin\subwcrev.exe" SET SUBWCREV="%ProgramFiles(x86)%\TortoiseSVN\bin\subwcrev.exe"
 IF EXIST "%ProgramFiles%\TortoiseSVN\bin\subwcrev.exe"      SET SUBWCREV="%ProgramFiles%\TortoiseSVN\bin\subwcrev.exe"
 IF EXIST "%ProgramW6432%\TortoiseSVN\bin\subwcrev.exe" SET SUBWCREV="%ProgramW6432%\TortoiseSVN\bin\subwcrev.exe"
 
-IF NOT EXIST %SUBWCREV% GOTO SKIPSUBWCREV
+IF NOT EXIST %SUBWCREV% (
+   ECHO subwcrev.exe not found in expected locations, skipping generation
+   GOTO SKIPSUBWCREV
+)
 
-%SUBWCREV% %CWD%xbmc/xbox/svn_rev.tmpl %REV_FILE% -f
+%SUBWCREV% %SVN_TEMPLATE% %REV_FILE% -f
 
-IF NOT EXIST %REV_FILE% %SUBWCREV% %CWD% %CWD%xbmc\xbox\svn_rev.tmpl %REV_FILE% -f
+REM Generate the SVN revision header if it does not exist
+IF NOT EXIST %REV_FILE% (
+   ECHO Generating SVN revision header from SVN repo
+   %SUBWCREV% "%CWD%." %SVN_TEMPLATE% %REV_FILE% -f
+)
 
 :SKIPSUBWCREV
 
-IF NOT EXIST %REV_FILE% copy %CWD%xbmc\xbox\svn_rev.unknown %REV_FILE%
+REM Copy the default unknown revision header if the generation did not occur
+IF NOT EXIST %REV_FILE% (
+   ECHO using default svn revision unknown header
+   copy %CWD%xbmc\xbox\svn_rev.unknown %REV_FILE%
+)
 
 SET REV_FILE=
 SET SUBWCREV=
