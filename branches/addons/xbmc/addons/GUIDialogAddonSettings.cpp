2a1e5dac70029b2be6dc669b4962d461377c29e5
buzz <buzz@568bbfeb-2a22-0410-94d2-cc84cf5bfa90>
2013-08-03 16:37:00 +0000

    merged: xbmc mainline d51895e1 - fixed: pull up viz settings from full screen viz window again fixed: error in last commit

--- branches/addons/xbmc/addons/GUIDialogAddonSettings.cpp
+++ branches/addons/xbmc/addons/GUIDialogAddonSettings.cpp
@@ -22,6 +22,7 @@
 #include "GUIDialogAddonSettings.h"
 #include "FileSystem/PluginDirectory.h"
 #include "utils/IAddon.h"
+#include "utils/AddonManager.h"
 #include "dialogs/GUIDialogNumeric.h"
 #include "dialogs/GUIDialogFileBrowser.h"
 #include "dialogs/GUIDialogOK.h"
@@ -102,6 +103,14 @@ bool CGUIDialogAddonSettings::OnMessage(CGUIMessage& message)
 void CGUIDialogAddonSettings::OnInitWindow()
 {
   FreeControls();
+  if (!m_addon)
+  { //we're looking for viz settings //FIXME
+    if (CAddonMgr::Get()->GetDefault(ADDON_VIZ, m_addon))
+    {
+      m_strHeading.Format("$LOCALIZE[10004] - %s", m_addon->Name().c_str()); // "Settings - AddonName"
+      m_addon->LoadSettings();
+    }
+  }
   CreateControls();
   CGUIDialogBoxBase::OnInitWindow();
 }
@@ -364,7 +373,7 @@ void CGUIDialogAddonSettings::CreateControls()
   CGUIImage *pOriginalImage = (CGUIImage *)GetControl(CONTROL_DEFAULT_SEPARATOR);
   CGUILabelControl *pOriginalLabel = (CGUILabelControl *)GetControl(CONTROL_DEFAULT_LABEL_SEPARATOR);
 
-  if (!pOriginalSpin || !pOriginalRadioButton || !pOriginalButton || !pOriginalImage)
+  if (!m_addon || !pOriginalSpin || !pOriginalRadioButton || !pOriginalButton || !pOriginalImage)
     return;
 
   pOriginalSpin->SetVisible(false);
@@ -673,6 +682,9 @@ bool CGUIDialogAddonSettings::TranslateSingleString(const CStdString &strConditi
 // Go over all the settings and set their default values
 void CGUIDialogAddonSettings::SetDefaults()
 {
+  if(!m_addon)
+    return;
+
   int controlId = CONTROL_START_CONTROL;
   const TiXmlElement *setting = m_addon->GetSettingsXML()->FirstChildElement("setting");
   while (setting)
