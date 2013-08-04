ff68bdc8e74c9f368efaba2ec42a4c33e183581a
buzz <buzz@568bbfeb-2a22-0410-94d2-cc84cf5bfa90>
2013-08-04 16:31:46 +0000

    merged: xbmc mainline f5f0b83e - removed: unused function

--- branches/addons/xbmc/FileSystem/PluginDirectory.h
+++ branches/addons/xbmc/FileSystem/PluginDirectory.h
@@ -45,7 +45,6 @@ public:
   virtual bool IsAllowed(const CStdString &strFile) const { return true; };
   static bool RunScriptWithParams(const CStdString& strPath);
   static bool HasPlugins(const CONTENT_TYPE &type);
-  bool GetPluginsDirectory(const CONTENT_TYPE &type, CFileItemList &items);
   bool StartScript(const CStdString& strPath);
   static bool GetPluginResult(const CStdString& strPath, CFileItem &resultItem);
 
