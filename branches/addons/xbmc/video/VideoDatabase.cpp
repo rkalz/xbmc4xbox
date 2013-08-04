462e2fe835e26699f7dffc102dc66f569d70418d
buzz <buzz@568bbfeb-2a22-0410-94d2-cc84cf5bfa90>
2013-08-04 22:55:32 +0000

    merged: xbmc mainline 680ac792 - cleanup: Get rid of one of the GetScraperForPath()'s and document it.

--- branches/addons/xbmc/video/VideoDatabase.cpp
+++ branches/addons/xbmc/video/VideoDatabase.cpp
@@ -5597,14 +5597,8 @@ int CVideoDatabase::GetMusicVideoCount(const CStdString& strWhere)
 
 ScraperPtr CVideoDatabase::GetScraperForPath( const CStdString& strPath )
 {
-  int iDummy;
-  return GetScraperForPath(strPath, iDummy);
-}
-
-ScraperPtr CVideoDatabase::GetScraperForPath(const CStdString& strPath, int& iFound)
-{
   SScanSettings settings;
-  return GetScraperForPath(strPath, settings, iFound);
+  return GetScraperForPath(strPath, settings);
 }
 
 ScraperPtr CVideoDatabase::GetScraperForPath(const CStdString& strPath, SScanSettings& settings)
@@ -7636,7 +7630,8 @@ void CVideoDatabase::ExportToXML(const CStdString &path, bool singleFiles /* = f
       for( map<CStdString,SScanSettings>::iterator iter=paths.begin();iter != paths.end();++iter)
       {
         int iFound=0;
-        ScraperPtr info = GetScraperForPath(iter->first,iFound);
+        SScanSettings settings;
+        ScraperPtr info = GetScraperForPath(iter->first, settings, iFound);
         if (info && iFound == 1)
         {
           TiXmlElement xmlPathElement2("path");
