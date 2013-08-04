089b8079e8a1b6d25af2880b5454ad0ee30beffb
buzz <buzz@568bbfeb-2a22-0410-94d2-cc84cf5bfa90>
2013-08-04 19:07:40 +0000

    merged: xbmc mainline 3b02eb2c - cleanup: Reduced code duplication in the RetrieveInfoFor* functions

--- xbmc/video/VideoInfoScanner.h
+++ xbmc/video/VideoInfoScanner.h
@@ -108,6 +108,23 @@ namespace VIDEO
     int RetreiveInfoForMovie(CFileItemPtr pItem, bool bDirNames, ADDON::ScraperPtr &scraper, bool bRefresh, CScraperUrl* pURL, CGUIDialogProgress* pDlgProgress, bool ignoreNfo);
     int RetreiveInfoForMusicVideo(CFileItemPtr pItem, bool bDirNames, ADDON::ScraperPtr &scraper, bool bRefresh, CScraperUrl* pURL, CGUIDialogProgress* pDlgProgress, bool ignoreNfo);
 
+    /*! \brief Update the progress bar with the heading and line and check for cancellation
+     \param progress CGUIDialogProgress bar
+     \param heading string id of heading
+     \param line1   string to set for the first line
+     \return true if the user has cancelled the scanner, false otherwise
+     */
+    bool ProgressCancelled(CGUIDialogProgress* progress, int heading, const CStdString &line1);
+
+    /*! \brief Find a url for the given video using the given scraper
+     \param videoName name of the video to lookup
+     \param scraper scraper to use for the lookup
+     \param url [out] returned url from the scraper
+     \param progress CGUIDialogProgress bar
+     \return >0 on success, <0 on failure, and 0 on no info found
+     */
+    int FindVideo(const CStdString &videoName, const ADDON::ScraperPtr &scraper, CScraperUrl &url, CGUIDialogProgress *progress);
+
     /*! \brief Extract episode and season numbers from a processed regexp
      \param reg Regular expression object with at least 2 matches
      \param episodeInfo Episode information to fill in.
