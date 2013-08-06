9cc63611b3ce99e79c42630f58c6e0a4d3dc6829
buzz <buzz@568bbfeb-2a22-0410-94d2-cc84cf5bfa90>
2013-08-06 21:46:58 +0000

    merged: xbmc mainline 2200d977 - fixed: temp fix for compile

--- xbmc/addons/Repository.cpp
+++ xbmc/addons/Repository.cpp
@@ -181,7 +181,7 @@ void CRepository::SetUpdated(const CDateTime& time)
   database.SetRepoTimestamp(ID(),time.GetAsDBDateTime());
 }
 
-CRepositoryUpdateJob::CRepositoryUpdateJob(RepositoryPtr& repo)
+CRepositoryUpdateJob::CRepositoryUpdateJob(RepositoryPtr& repo, bool check)
 {
   m_repo = boost::dynamic_pointer_cast<CRepository>(repo->Clone(repo));
 }
