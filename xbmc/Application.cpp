b67f3e459cb9d01e7f16be4a57db58950320ab41
buzz <buzz@568bbfeb-2a22-0410-94d2-cc84cf5bfa90>
2013-08-07 13:40:13 +0000

    merged: xbmc mainline 5618de9f - changed: shutdown XBMC when no skins are found

--- xbmc/Application.cpp
+++ xbmc/Application.cpp
@@ -1220,8 +1220,15 @@ HRESULT CApplication::Initialize()
 
   g_windowManager.Add(new CGUIWindowHome);                     // window id = 0
 
+  // Make sure we have at least the default skin
   if (!LoadSkin(g_guiSettings.GetString("lookandfeel.skin")))
-    LoadSkin(DEFAULT_SKIN);
+  {
+    if (!LoadSkin(DEFAULT_SKIN))
+    {
+      CLog::Log(LOGERROR, "Default skin '%s' not found! Terminating..", DEFAULT_SKIN);
+      FatalErrorHandler(true, false, true);
+    }
+  }
 
   g_windowManager.Add(new CGUIWindowPrograms);                 // window id = 1
   g_windowManager.Add(new CGUIWindowPictures);                 // window id = 2
