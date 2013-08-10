7ceb6fdcc1093c99cb6a509b461bd3c432bcae5c
buzz <buzz@568bbfeb-2a22-0410-94d2-cc84cf5bfa90>
2013-08-10 19:00:38 +0000

    merged: xbmc mainline 89bb5945 - cleanup: Remove orphaned function prototype

--- xbmc/utils/ScraperUrl.h
+++ xbmc/utils/ScraperUrl.h
@@ -78,7 +78,6 @@ public:
   static bool Get(const SUrlEntry&, std::string&, XFILE::CCurlFile& http,
                  const CStdString& cacheContext);
   static bool DownloadThumbnail(const CStdString &thumb, const SUrlEntry& entry);
-  static void ClearCache();
 
   CStdString m_xml;
   CStdString m_spoof; // for backwards compatibility only!
