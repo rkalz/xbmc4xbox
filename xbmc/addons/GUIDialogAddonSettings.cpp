ba0f6e4e3d101bd1031272608baac2ed7d50f69d
buzz <buzz@568bbfeb-2a22-0410-94d2-cc84cf5bfa90>
2013-08-10 17:43:34 +0000

    merged: xbmc mainline 22ba6ec6 - added: Possibility to just update the local instance of settings when changes are made in the addon settings dialog.

--- xbmc/addons/GUIDialogAddonSettings.cpp
+++ xbmc/addons/GUIDialogAddonSettings.cpp
@@ -139,7 +139,7 @@ void CGUIDialogAddonSettings::OnInitWindow()
 }
 
 // \brief Show CGUIDialogOK dialog, then wait for user to dismiss it.
-bool CGUIDialogAddonSettings::ShowAndGetInput(const AddonPtr &addon)
+bool CGUIDialogAddonSettings::ShowAndGetInput(const AddonPtr &addon, bool saveToDisk /* = true */)
 {
   if (!addon)
     return false;
@@ -160,6 +160,7 @@ bool CGUIDialogAddonSettings::ShowAndGetInput(const AddonPtr &addon)
 
     pDialog->m_changed = false;
     pDialog->m_addon = addon;
+    pDialog->m_saveToDisk = saveToDisk;
     pDialog->DoModal();
     ret = true;
   }
@@ -409,7 +410,8 @@ void CGUIDialogAddonSettings::SaveSettings(void)
   for (map<CStdString, CStdString>::iterator i = m_settings.begin(); i != m_settings.end(); ++i)
     m_addon->UpdateSetting(i->first, i->second);
 
-  m_addon->SaveSettings();
+  if (m_saveToDisk)
+    m_addon->SaveSettings();
 }
 
 void CGUIDialogAddonSettings::FreeSections()
