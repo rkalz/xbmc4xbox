9973ee6f76efd44912384f7a4376bce9ec68de19
buzz <buzz@568bbfeb-2a22-0410-94d2-cc84cf5bfa90>
2013-08-10 21:55:43 +0000

    merged: xbmc mainline 4648738a - fixed: Don't request updates from addon repositories every 10 minutes, rather every 6 hours.

--- xbmc/XBDateTime.h
+++ xbmc/XBDateTime.h
@@ -176,6 +176,11 @@ public:
    */
   void SetFromDBDateTime(const CStdString &dateTime);
 
+  /*! \brief set from a database datetime format YYYY-MM-DD HH:MM:SS
+   \sa GetAsDBDateTime()
+   */
+  void SetFromDBDateTime(const CStdString &dateTime);
+
   void GetAsSystemTime(SYSTEMTIME& time) const;
   void GetAsTime(time_t& time) const;
   void GetAsTm(tm& time) const;
