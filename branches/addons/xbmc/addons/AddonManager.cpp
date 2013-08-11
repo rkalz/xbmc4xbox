eff6e105a7de5f557a579294c06a547751d44f15
buzz <buzz@568bbfeb-2a22-0410-94d2-cc84cf5bfa90>
2013-08-11 16:06:52 +0000

    merged: xbmc mainline 9d4264e2a - added: Initial support for python module addons.

--- branches/addons/xbmc/addons/AddonManager.cpp
+++ branches/addons/xbmc/addons/AddonManager.cpp
@@ -94,6 +94,7 @@ AddonPtr CAddonMgr::Factory(const cp_extension_t *props)
     case ADDON_SCRIPT_LYRICS:
     case ADDON_SCRIPT_WEATHER:
     case ADDON_SCRIPT_SUBTITLES:
+    case ADDON_SCRIPT_MODULE:
       return AddonPtr(new CAddon(props));
     case ADDON_SCRAPER_ALBUMS:
     case ADDON_SCRAPER_ARTISTS:
@@ -450,6 +451,7 @@ AddonPtr CAddonMgr::AddonFromProps(AddonProps& addonProps)
     case ADDON_SCRIPT_LYRICS:
     case ADDON_SCRIPT_WEATHER:
     case ADDON_SCRIPT_SUBTITLES:
+    case ADDON_SCRIPT_MODULE:
       return AddonPtr(new CAddon(addonProps));
     case ADDON_SCRAPER_ALBUMS:
     case ADDON_SCRAPER_ARTISTS:
