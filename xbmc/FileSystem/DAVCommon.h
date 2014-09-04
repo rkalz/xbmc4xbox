30c49dc4713dd956a254c67ab4af4ebd1aee2051
buzz <buzz@568bbfeb-2a22-0410-94d2-cc84cf5bfa90>
2014-09-04 23:46:43 +0000

    merged: xbmc mainline 1edf00ac - ADD: filesystems: Write-enabled WEBDAV

--- xbmc/FileSystem/DAVCommon.h
+++ xbmc/FileSystem/DAVCommon.h
@@ -1,7 +1,7 @@
 #pragma once
 /*
  *      Copyright (C) 2005-2013 Team XBMC
- *      http://xbmc.org
+ *      http://www.xbmc.org
  *
  *  This Program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -14,28 +14,21 @@
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with XBMC; see the file COPYING.  If not, see
- *  <http://www.gnu.org/licenses/>.
+ *  along with XBMC; see the file COPYING.  If not, write to
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  http://www.gnu.org/copyleft/gpl.html
  *
  */
 
-#include "IDirectory.h"
+#include "utils/StdString.h"
 #include "tinyXML/tinyxml.h"
-#include "FileItem.h"
 
 namespace XFILE
 {
-  class CDAVDirectory : public IDirectory
+  class CDAVCommon
   {
     public:
-      CDAVDirectory(void);
-      virtual ~CDAVDirectory(void);
-      virtual bool GetDirectory(const CStdString& strPath, CFileItemList &items);
-      virtual bool Create(const char* strPath);
-      virtual bool Exists(const char* strPath);
-      virtual bool Remove(const char* strPath);
-      virtual DIR_CACHE_TYPE GetCacheType(const CStdString& strPath) const { return DIR_CACHE_ONCE; };
-    private:
-      void ParseResponse(const TiXmlElement *pElement, CFileItem &item);
+      static bool ValueWithoutNamespace(const TiXmlNode *pNode, const CStdString& value);
+      static CStdString GetStatusTag(const TiXmlElement *pElement);
   };
 }
--- xbmc/FileSystem/File.cpp
+++ xbmc/FileSystem/File.cpp
@@ -530,6 +530,9 @@ void CFile::Close()
 {
   try
   {
+    if (m_pFile)
+      m_pFile->Close();
+
       SAFE_DELETE(m_pBuffer);
       SAFE_DELETE(m_pFile);
   }
