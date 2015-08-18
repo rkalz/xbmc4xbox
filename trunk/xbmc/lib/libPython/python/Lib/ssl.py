11b7d0e8f9c1a7cb60d1369b29221a202bf5baed
buzz <buzz@568bbfeb-2a22-0410-94d2-cc84cf5bfa90>
2015-08-18 02:09:05 +0000

    fixed: python ssl lib - don't try to load certs (we are not win32)

--- trunk/xbmc/lib/libPython/python/Lib/ssl.py
+++ trunk/xbmc/lib/libPython/python/Lib/ssl.py
@@ -134,8 +134,6 @@ except NameError:
     _SSLv2_IF_EXISTS = None
 
 from socket import socket, _fileobject, _delegate_methods, error as socket_error
-if sys.platform == "win32":
-    from _ssl import enum_certificates, enum_crls
 
 from socket import socket, AF_INET, SOCK_STREAM, create_connection
 from socket import SOL_SOCKET, SO_TYPE
@@ -386,9 +384,6 @@ class SSLContext(_SSLContext):
     def load_default_certs(self, purpose=Purpose.SERVER_AUTH):
         if not isinstance(purpose, _ASN1Object):
             raise TypeError(purpose)
-        if sys.platform == "win32":
-            for storename in self._windows_cert_stores:
-                self._load_windows_store_certs(storename, purpose)
         self.set_default_verify_paths()
 
 
